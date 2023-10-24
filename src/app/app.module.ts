6da39ef52cad36ae033db3deb95cf5e419350204
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2023-10-24 13:14:47 +0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,6 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { PostsFeedPageComponent } from './pages/posts-feed-page/posts-feed-page.component';
 import { SelectComponent } from './components/ui/select/select.component';
-import {environment} from "./enviroment";
 
 @NgModule({
   declarations: [
